9a9cb9dbe249ae648fd7521130bf90c4213e319d
Luzzardd <mnaufalhasyim112@gmail.com>
2024-11-25 10:20:27 +0700

    pertama

---
+++ src/components/Demo/Hero.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { HStack } from "@chakra-ui/react"
+
+const Hero = () => {
+    return (
+    <HStack>
+    <Button>Click me</Button>
+    <Button>Click me</Button>
+    </HStack>
+)
+}
+export default Hero
